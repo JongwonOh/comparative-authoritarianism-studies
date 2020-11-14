9392d590fdd1254cb619435f6784e87d169a7eb4
Jongwon Oh <73306263+JongwonOh@users.noreply.github.com>
2020-11-14 14:14:10 +0900

    Update military.R

--- military.R
+++ military.R
@@ -1,4 +1,3 @@
-library(foreign)
 library(survival)
 library(survminer)
 library(dplyr)
@@ -10,7 +9,6 @@ library(stargazer)
 military <- read.csv("D:/Jongwon's/Political Science/OH_Military_Data.csv")
 
 military$region <- factor(military$region)
-military$colonyof <- factor(military$colonyof)
 military$mpdummy <- factor(military$mpdummy)
 military$majordummy <- factor(military$majordummy)
 military$opec <- factor(military$opec)
@@ -28,6 +26,15 @@ quantile(model1,probs=c(0.25,0.5,0.75),conf.int=T)
 plot(model1,xlab='time',ylab='survival rate',mark.time=T)
 legend(0.5,0.2,c("Product-limit estimator","95% CI"),lty=c(1,2))
 ggsurvplot(model1,data=military)
+
+w_model1=survreg(formula=Surv(duryear,status)~1,data=military,dist="weibull")
+surv_prob <- seq(.99, .01, by = -.01)
+surv_time <- predict(w_model1, type = "quantile", p = 1 - surv_prob, 
+                     newdata = data.frame(1))
+model1_df <- tibble(time = surv_time, surv = surv_prob, upper = NA, lower = NA, 
+                    std.err = NA) %>% as.data.frame()
+ggsurvplot_df(fit=model1_df, surv.geom=geom_line, pval=TRUE, conf.int=TRUE, 
+              surv.median.line="v", break.time.by=5)
 
 capture.output(summary(model1), 
                file="D:/Jongwon's/Political Science/overall military.doc", 
@@ -47,74 +54,47 @@ capture.output(summary(model2),
                file="D:/Jongwon's/Political Science/kaplan maier party.doc", 
                append=TRUE)
 
+model2_2=survfit(Surv(duryear,status)~majordummy,data=military,
+                 conf.type="log-log")
+ggsurvplot(model2_2, data=military, conf.int=TRUE, surv.median.line="v",
+           break.time.by=5)
+
 # 3. multivariate analysis
 ## variable choice in Cox's model
-model3 <- coxph(Surv(duryear,status)~mpdummy+majordummy+loggdp1+loggpc10+gg100
-                +mgr100+opec+ethnic100+region,data=military)
+model3 <- coxph(Surv(duryear,status)~mpdummy+loggdp1+loggpc10+gg100+mgr100+opec
+                +ethnic100+region,data=military)
 summary(model3)
 ggforest(model3, data=military)
 
-model4 <- coxph(Surv(duryear,status)~mpdummy+majordummy+loggdp1+loggpc10+gg100, 
-                data = military)
+model4 <- coxph(Surv(duryear,status)~majordummy+loggdp1+loggpc10+gg100+mgr100
+                +opec+ethnic100+region,data=military)
 summary(model4)
+ggforest(model4, data=military)
 
-model5 <- coxph(Surv(duryear,status)~mpdummy+majordummy+mgr100,data=military)
-summary(model5)
-
-model6 <- coxph(Surv(duryear,status)~mpdummy+majordummy+region,data=military)
-summary(model6)
-
-model7 <- coxph(Surv(duryear,status)~mpdummy+majordummy+ethnic100,data=military)
-summary(model7)
-
-model8 <- coxph(Surv(duryear,status)~mpdummy+majordummy+opec,data=military)
-summary(model8)
-
-model9 <- coxph(Surv(duryear,status)~mpdummy+majordummy,data=military)
-summary(model9)
-
-stargazer(model9,model4,model5,model8,model7,model6,model3, type="html", 
-          digits=1, out="table1.doc",flip=TRUE)
+stargazer(model3,model4, type="html", digits=1, out="table1.doc",
+          flip=TRUE)
 
 # 4. linear regression duration and party
-duration1 <- lm(duryear~mpdummy+majordummy+loggdp1+loggpc10+gg100+mgr100+opec+
-                        region+ethnic100,data=military)
+duration1 <- lm(duryear~mpdummy+loggdp1+loggpc10+gg100+mgr100+opec+region+
+                        ethnic100,data=military)
 car::vif(duration1)
 sqrt(car::vif(duration1)) > 2
 summary(duration1)
 
-duration2 <- lm(duryear~mpdummy+majordummy+loggdp1+loggpc10+gg100,data=military)
+duration2 <- lm(duryear~seat100_1+loggdp1+loggpc10+gg100+mgr100+opec+
+                        region+ethnic100,data=military)
 car::vif(duration2)
 sqrt(car::vif(duration2)) > 2
 summary(duration2)
 
-duration3 <- lm(duryear~mpdummy+majordummy+mgr100,data=military)
+duration3 <- lm(duryear~majordummy+loggdp1+loggpc10+gg100+mgr100+opec+
+                        region+ethnic100,data=military)
 car::vif(duration3)
 sqrt(car::vif(duration3)) > 2
 summary(duration3)
 
-duration4 <- lm(duryear~mpdummy+majordummy+opec, data = military)
-car::vif(duration4)
-sqrt(car::vif(duration4)) > 2
-summary(duration4)
-
-duration5 <- lm(duryear~mpdummy+majordummy+region, data = military)
-car::vif(duration5)
-sqrt(car::vif(duration5)) > 2
-summary(duration5)
-
-duration6 <- lm(duryear~mpdummy+majordummy+ethnic100, data = military)
-car::vif(duration6)
-sqrt(car::vif(duration6)) > 2
-summary(duration6)
-
-duration7 <- lm(duryear~mpdummy+majordummy, data = military)
-car::vif(duration7)
-sqrt(car::vif(duration7)) > 2
-summary(duration7)
-
-stargazer(duration7,duration2,duration3,duration4,duration6,duration5,duration1,
-          type="html", digits=1, out="table2.doc", flip=TRUE)
+stargazer(duration1,duration2,duration3,type="html", digits=1, out="table2.doc", 
+          flip=TRUE)
 
 plot(military$mpdummy, military$duryear, cex=.25, type="n", 
      xlab = "Military Party or Not", ylab = "Regime Duration")
@@ -123,45 +103,25 @@ abline(lm(military$duryear ~ military$mpdummy), lty=2, col="red",
        untf = TRUE)
 
 #5. linear regression seat share
-seat1 <- lm(seat100_1~yeardiff+seat100_2+opposnum+timediff+loggdp1+loggpc10+
-                    gg100+mgr100+opec+ethnic100,data=military)
+seat1 <- lm(seat100_1~yeardiff+timediff+loggdp1+loggpc10+gg100+mgr100+opec
+            +ethnic100,data=military)
 car::vif(seat1)
 sqrt(car::vif(seat1)) > 2
 summary(seat1)
 
-seat2 <- lm(seat100_1~yeardiff+seat100_2+opposnum+loggdp1+loggpc10+gg100,
-            data=military)
+seat2 <- lm(seat100_1~yeardiff+seat100_2+timediff+loggdp1+loggpc10+gg100+mgr100
+            +opec+ethnic100,data=military)
 car::vif(seat2)
-sqrt(car::vif(seat2)) > 2
+sqrt(car::vif(seat1)) > 2
 summary(seat2)
 
-seat3 <- lm(seat100_1~yeardiff+seat100_2+opposnum+mgr100,data=military)
+seat3 <- lm(seat100_1~yeardiff+seat100_3+timediff+loggdp1+loggpc10+gg100+mgr100
+            +opec+ethnic100,data=military)
 car::vif(seat3)
 sqrt(car::vif(seat3)) > 2
 summary(seat3)
 
-seat4 <- lm(seat100_1~yeardiff+seat100_2+opposnum+opec,data=military)
-car::vif(seat4)
-sqrt(car::vif(seat4)) > 2
-summary(seat4)
-
-seat5 <- lm(seat100_1~yeardiff+seat100_2+opposnum+ethnic100,data=military)
-car::vif(seat5)
-sqrt(car::vif(seat5)) > 2
-summary(seat5)
-
-seat6 <- lm(seat100_1~yeardiff+seat100_2+opposnum+timediff,data=military)
-car::vif(seat6)
-sqrt(car::vif(seat6)) > 2
-summary(seat6)
-
-seat7 <- lm(seat100_1~yeardiff+seat100_2+opposnum,data=military)
-car::vif(seat7)
-sqrt(car::vif(seat7)) > 2
-summary(seat7)
-
-stargazer(seat7,seat2,seat3,seat4,seat5,seat6,seat1, type="html", digits=1, 
-          out="table3.doc", flip=TRUE)
+stargazer(seat1,seat2,seat3, type="html", digits=1, out="table3.doc", flip=TRUE)
 
 plot(military$yeardiff, military$seat100_1, cex=.25, type="n", 
      xlab = "Year Difference between Nation Building and Military Dictatorship", 
@@ -172,6 +132,6 @@ abline(lm(military$seat100_1 ~ military$yeardiff), lty=2, col="red",
 
 military2 <- read.csv("D:/Jongwon's/Political Science/OH_Military_Data(2).csv")
 stargazer(military2, type = "html", 
-          summary.stat = c("min", "median", "max", "sd", "mean","n"), 
+          summary.stat = c("min", "median", "max", "mean", "sd","n"), 
           title = "Descriptive Statistics", 
           digits = 1, out = "descriptive statistics.doc", flip = TRUE)
